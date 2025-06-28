19511f3a72590c10a16e43769cfaff0cc743e141
chollasit-dev <118034127+chollasit-dev@users.noreply.github.com>
2025-06-28 22:48:52 +0700

    Add Zsh syntax highlighting init script
    This is specific to installation via Pacman since the package path is
    different from others.

---
+++ init/syntax-highlighting.zsh
@@ -0,0 +1,7 @@
+# shellcheck disable=SC2148
+
+# https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/INSTALL.md
+if [ -f "/usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ]; then
+  # shellcheck source=/dev/null
+  source "/usr/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+fi
