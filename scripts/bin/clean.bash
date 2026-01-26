671b3368cbfe714266c01692a98cc68ab6141961
chollasit-dev <118034127+chollasit-dev@users.noreply.github.com>
2026-01-27 01:14:30 +0700

    Refactor script to prevent unexpected exit on failed

--- scripts/bin/clean.bash
+++ scripts/bin/clean.bash
@@ -7,7 +7,7 @@ install_aur_package_manager &&
   yay -Scc --noconfirm &&
   yay -Ycc --noconfirm
 
-rm "$HOME/.zsh_history" "$HOME/.bash_history"
-rm -rf "$HOME/.local/share/Trash/*"
+rm "$HOME/.zsh_history" "$HOME/.bash_history" || :
+rm -rf "$HOME/.local/share/Trash/*" || :
 
 clipse -clear-all
