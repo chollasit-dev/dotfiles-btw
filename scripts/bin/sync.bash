7ccabbaa3ffaafd7601fbc5b7a943f2790960c47
chollasit-dev <118034127+chollasit-dev@users.noreply.github.com>
2026-02-01 02:48:00 +0700

    Refactor sync script
    - Add check whether to fetch git submodule or not if exists
    - Add option to import existing files into stow packages (--adopt)
    - Improve error handling with list operator `||` chaining with exit
      statement
    - Add essential status message logging for quick analysis

--- scripts/bin/sync.bash
+++ scripts/bin/sync.bash
@@ -5,6 +5,22 @@ set -eu -o pipefail
 # shellcheck source=../shared.bash
 source "$(dirname "$(dirname "$0")")/shared.bash"
 
+fetch_remote() {
+  {
+    [ -d "$1" ] &&
+      cd "$1" &&
+      git pull origin main --set-upstream &&
+      [ ! -f "./.gitmodules" ] || {
+      echo "[sync.bash] Submodule repository found, try updating..." &&
+        git submodule update --remote ||
+        echo "[sync.bash] Failed to update submodule from remote at parent $1"
+    }
+  } || {
+    echo "[sync.bash] Failed to fetch update from remote for $1" >&2 &&
+      return 1
+  }
+}
+
 DIRS=(
   "$HOME/Documents/"
   "$HOME/dotfiles-arch/"
@@ -13,30 +29,41 @@ DIRS=(
   "$HOME/shared-configs/"
 )
 
-read -p "Should resync symbolic links (recommended for new files)? [y/n] " -r option
-case "$option" in
+read -p "Should resync symlinks (recommended for new files)? [y/n] " -r resync_symlink_opts
+case "$resync_symlink_opts" in
 "y" | "Y")
   for dir in "${DIRS[@]}"; do
-    if [ -d "$dir" ]; then
-      cd "$dir" && git pull origin main --set-upstream
-      if [ -f "$dir/.stow-local-ignore" ]; then
-        install_symlink_farm
-        stow -Rv . --no-folding
+    fetch_remote "$dir" || continue
+    {
+      command -v stow &>/dev/null || {
+        echo "[sync.bash] GNU Stow not found, skip resyncing symlinks..." >&2 && continue
+      }
+      [ -f "$dir/.stow-local-ignore" ] || {
+        echo "[sync.bash] Stow ignore file not found" >&2 &&
+          echo "[sync.bash] This may not meant to be stowed, skip resyncing symlinks at $dir" >&2 &&
+          continue
+      }
+      if [ "$(git status --short | wc -l)" != 0 ]; then
+        git status --short &&
+          echo "[sync.bash] Working directory $dir is dirty" &&
+          read -p "Import existing files into stow package? [y/n] " -r stow_adopt_opts
+        case "$stow_adopt_opts" in
+        "y" | "Y") stow . --no-folding --restow --verbose --adopt ;;
+        "n" | "N") stow . --no-folding --restow --verbose ;;
+        esac
       fi
-    fi
+    } || echo "[sync.bash] Failed to restow symlinks at $dir" >&2
   done
   ;;
 
 "n" | "N")
   for dir in "${DIRS[@]}"; do
-    [ -d "$dir" ] &&
-      cd "$dir" &&
-      git pull origin main --set-upstream
+    fetch_remote "$dir" || continue
   done
   ;;
 
 *)
-  echo "Invalid option" && exit 1
+  echo "[sync.bash] Invalid option" >&2 && exit 1
   ;;
 esac
 
